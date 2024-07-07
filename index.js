dfcf0ddaabc57c13409d700dba418ec27d293ec6
murilo-l1 <murilolusvarghi@alunos.utfpr.edu.br>
2024-07-07 19:31:48 -0300

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-let date;
-let unix;
 // cors enabled so your API is remotely testable by FCC
 const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
@@ -23,15 +21,32 @@ app.get("/api/hello", function (req, res) {
 
 //empty date case
 app.get('/api/', (req, res) => {
-  date = new Date().toUTCString();
-  unix = Date.now();
+  const date = new Date().toUTCString();
+  const unix = Date.now();
   res.json({unix: unix, utc: date});
 });
 
+// case when a valid date/unix time is passed as endpoint
 app.get('/api/:date', (req, res) => {
+  let date;
+  const requestedDate = req.params['date'];
+
+  // if is a unix value treat as a number
+  if(Number.isInteger(Number(requestedDate))){
+    date = new Date(Number(requestedDate));
+  }else{
+    date = new Date(requestedDate);
+  }
+
+  if(isNaN(date.getTime())){
+    res.json({error: "Invalid date"});
+  }else {
+    const unixTime = date.getTime();
+    const formattedDate = date.toUTCString();
+    res.json({unix: unixTime, utc: formattedDate});
+  }
 
 });
-
 
 
 const listener = app.listen(process.env.PORT || 3000, function () {
